6406fa81c8031121174919d4af18400b6350d087
Mahhek <mahhek.khan@gmail.com>
2025-06-16 10:47:08 +0500

    remove model dependency

--- app/models/data_fields/model.rb
+++ app/models/data_fields/model.rb
@@ -2,6 +2,7 @@ module DataFields
   class Model < Base
     data_attribute :value, :string
     metadata_attribute :class_name, :string, default: ""
+
     validates :value, presence: true, if: -> { data_value? && required? }, on: :update
     validate :value_is_correct_class, if: -> { data_value? }, on: :update
 
@@ -17,15 +18,14 @@ module DataFields
 
     def definition_requires_options? = true
 
-    def self.model_types = MODEL_TYPES
+    def self.model_types = []
 
-    private def value_is_correct_class
-      errors.add :value, :invalid if value.present? && class_name.present? && !value.is_a?(class_name.constantize)
+    private
+
+    def value_is_correct_class
+      if value.present? && class_name.present? && !value.is_a?(class_name.constantize)
+        errors.add :value, :invalid
+      end
     end
-
-    ModelTypeName = Data.define(:class_name, :name)
-
-    MODEL_TYPES = [Customer, Service, StaffGroup, User].map { |model_type| ModelTypeName.new(class_name: model_type.to_s, name: model_type.model_name.human) }.freeze
-    private_constant :MODEL_TYPES, :ModelTypeName
-end
+  end
 end
